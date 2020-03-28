0ce3ac2448cad8a917c9c00b4abef4bd6899b5b5
cobs14 <44751106+cobs14@users.noreply.github.com>
2020-03-29 04:21:11 +0800

    Revert "improved move list"
    This reverts commit 2830cefc2e85da07072a90a2c96ee88a64d037f6.

--- renju/createmoves.h
+++ renju/createmoves.h
@@ -8,9 +8,7 @@
 #include <unordered_map>
 #include <vector>
 
-// 这里要保持它是偶数，详见对它的引用(createsMove)
-// 或者修改时记得将createsMove里的相应部分修改合理
-#define MOVE_NUM (8U) 
+#define MOVE_NUM (5U)
 
 extern std::unordered_map<hash_t, std::vector<point>> createdMoves;
 
