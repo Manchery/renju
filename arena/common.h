02e02da7e36ffcf8f55c0db37528400e1c8d6b77
Jialong Wu <Manchery@outlook.com>
2020-04-18 12:45:09 +0800

    add pk

---
+++ arena/common.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define blank (0)												//空白点
+#define black (1)												//黑方
+#define white (2)												//白方
+#define draw  (3)												//平局
+#define opposite(x) (3-(x))										//对手：black->white, white->black
+#define inf (1<<30)												//常数正无穷
+#define winValue (100000000)									//必胜局面的赋值
+#define inboard(a,b) ((a)>0 && (a)<=15 && (b)>0 && (b)<=15)		//用于检验招法是否在棋盘上
+#define GRID_NUM 16												//棋盘规模	
+
+//棋盘坐标的点对
+struct point {
+	int x, y;
+	point(int x = 0, int y = 0) :x(x), y(y) {}
+};
+bool operator ==(const point& A, const point& B)
+{
+	return A.x == B.x && A.y == B.y;
+}
---
+++ arena/pk.cpp
@@ -0,0 +1,152 @@
+#include "AlphaBeta.h"
+#include "HashMap.h"
+#include "IDSearch.h"
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <cassert>
+using namespace std;
+#define cl(x) memset(x,0,sizeof(x));
+
+int chessBoard[GRID_NUM][GRID_NUM];
+int remainBlank;
+
+//打印棋盘
+void print();
+int gameover();
+void Print(int round, int blackWin, int whiteWin, string blackName, string whiteName, int current);
+
+#define AgentBlack IDSearch
+string blackName("IDSearch");
+#define AgentWhite AlphaBeta
+string whiteName("AlphaBeta");
+const int Round = 20;
+
+int main() {
+	int blackWin = 0, whiteWin = 0; int winner;
+	for (int i = 1; i <= Round; i++) {
+		cl(chessBoard); remainBlank = 225;
+		AgentBlack::clearAll(); AgentBlack::initHashValue();
+		AgentWhite::clearAll(); //AgentWhite::initHashValue();
+		AgentBlack::agent = AgentWhite::user = black;
+		AgentWhite::agent = AgentBlack::user = white;
+		// AgentWhite::zobrist ^= AgentWhite::MinFirstValue;
+		while (true) {
+			Print(i, blackWin, whiteWin, blackName, whiteName, black);
+			point blackPos = AgentBlack::searchMove().first;
+			if (chessBoard[blackPos.x][blackPos.y] != blank) {
+				cout << "黑方落子非法" << endl; system("pause"); return 0;
+			}
+			chessBoard[blackPos.x][blackPos.y] = black; remainBlank--;
+			AgentBlack::makeMove(blackPos, AgentBlack::agent);
+			AgentWhite::makeMove(blackPos, AgentBlack::user);
+			if (winner = gameover()) {
+				if (winner == black) blackWin++;
+				break;
+			}
+
+			Print(i, blackWin, whiteWin, blackName, whiteName, white);
+			point whitePos = AgentWhite::searchMove().first;
+			if (chessBoard[whitePos.x][whitePos.y] != blank) {
+				cout << "白方落子非法" << endl; system("pause"); return 0;
+			}
+			chessBoard[whitePos.x][whitePos.y] = white; remainBlank--;
+			AgentWhite::makeMove(whitePos, AgentBlack::agent);
+			AgentBlack::makeMove(whitePos, AgentBlack::user);
+			if (winner = gameover()) {
+				if (winner == white) whiteWin++;
+				break;
+			}
+		}
+	}
+	system("pause");
+	return 0;
+}
+
+void Print(int round, int blackWin, int whiteWin, string blackName, string whiteName, int current) {
+	system("cls");
+	cout << "黑方：" << blackName << "；白方：" << whiteName << endl;
+	printf("第 %d/%d 局\n黑方已胜:白方已胜:平局 = %d:%d:%d\n", round, Round, blackWin, whiteWin, round - 1 - blackWin - whiteWin);
+	print();
+	cout << (current == black ? "黑棋" : "白棋") << "思考中..." << endl;
+}
+
+//生成棋盘形状
+void judge(int i, int j)
+{
+	if (i == 1 && j == 1)
+		printf("╔ ");
+	else if (i == 1 && j == 15)
+		printf("╗");
+	else if (i == 15 && j == 1)
+		printf("╚ ");
+	else if (i == 15 && j == 15)
+		printf("╝");
+	else if (i == 1 && j != 15 && j != 1)
+		printf("┯ ");
+	else if (i == 15 && j != 15 && j != 1)
+		printf("┷ ");
+	else if (j == 1 && i != 15 && i != 1)
+		printf("┠ ");
+	else if (j == 15 && i != 15 && i != 1)
+		printf("┨");
+	else
+		printf("┼ ");
+}
+
+//打印棋盘
+void print()
+{
+	for (int i = 0; i < 16; i++)
+	{
+		if (i == 0)
+		{
+			printf("  ");
+			for (int j = 1; j < 16; j++)
+				printf("%-2d", j);
+			printf("\n");
+			continue;
+		}
+		for (int j = 0; j < 16; j++)
+		{
+			if (j == 0)
+			{
+				printf("%2d", i);
+				continue;
+			}
+
+			if (chessBoard[i][j] == 1) //此处为黑子
+				printf("○");
+			else if (chessBoard[i][j] == 2) //此处为白子
+				printf("●");
+			else
+			{
+				judge(i, j);
+			}
+		}
+		printf("\n");
+	}
+}
+
+static constexpr int Dx[] = { 1,0,1,1 };						//连子方向
+static constexpr int Dy[] = { 0,1,1,-1 };
+
+//完整计算游戏结束时的状态
+int gameover()
+{
+	if (!remainBlank) return draw; //平局
+	for (int i = 1; i < GRID_NUM; i++) {
+		for (int j = 1; j < GRID_NUM; j++) {
+			if (chessBoard[i][j] == blank)
+				continue;
+			for (int k = 0; k < 4; k++) {
+				bool same = 1; int dx = Dx[k], dy = Dy[k];
+				for (int v = -2; v <= 2; v++)
+					same &= (inboard(i + dx * v, j + dy * v) && chessBoard[i + dx * v][j + dy * v] == chessBoard[i][j]);
+				if (same)
+					return chessBoard[i][j]; //某一方胜利的计算
+			}
+		}
+	}
+	return blank;
+}
